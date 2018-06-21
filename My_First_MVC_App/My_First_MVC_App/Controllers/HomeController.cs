40f848a2f7b07693101c3dfc1e1ecc41fce0d958
NateA275 <nateaurich@gmail.com>
2018-06-21 12:42:56 -0700

    Updated Comments in HomeController

--- My_First_MVC_App/My_First_MVC_App/Controllers/HomeController.cs
+++ My_First_MVC_App/My_First_MVC_App/Controllers/HomeController.cs
@@ -9,18 +9,37 @@ namespace My_First_MVC_App.Controllers
 {
     public class HomeController : Controller
     {
+
+        /// <summary>
+        /// Index - Retruns the default IActionResult View
+        /// </summary>
+        /// <returns> IActionResult - Index.cshtml </returns>
         [HttpGet]
-        public IActionResult Index(string name, int number)
+        public IActionResult Index()
         {
             return View();
         }
 
+
+        /// <summary>
+        /// Index - Retrieves beginning year and end year from index page
+        /// </summary>
+        /// <param name="begYear"> int - User's entered value in beginning year box </param>
+        /// <param name="endYear"> int - User's entered value in ending year box </param>
+        /// <returns>IActionResult - Results Page</returns>
         [HttpPost]
         public IActionResult Index(int begYear, int endYear)
         {
             return RedirectToAction("Results", new { begYear, endYear });
         }
 
+
+        /// <summary>
+        /// Results - Handle's Index redirection
+        /// </summary>
+        /// <param name="begYear"> int - User's entered value in beginning year box </param>
+        /// <param name="endYear"> int - User's entered value in ending year box </param>
+        /// <returns>IActionReuslt - Results View </returns>
         public IActionResult Results(int begYear, int endYear)
         {
             Recipient recipients = new Recipient();
